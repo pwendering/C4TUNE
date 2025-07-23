da9e84a1990dc4a1969db7a91a1fd0fba9d8f63e
pwendering <philipp.wendering@gmail.com>
2025-07-23 16:00:58 +0200

    Add global init file

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+__version__ = "1.0.0"
+__author__ = "Philipp Wendering"
+
+from .data import data
+from .losses import losses
+from .models import model_c4tune, model_surrogate
+from .training import base_trainer, c4tune_trainer, surrogate_trainer
+from .prediction import base_predictor, c4tune_predictor, surrogate_predictor
+from .utils import utils, data_stats, env_setup, experiment_logger, input_transform
+from .c4_kinetic_model import c4model
+from .losses import losses
